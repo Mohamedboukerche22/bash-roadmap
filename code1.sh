18567c5420aebbea05d1b32383c9494bd102bfa8
Mohamed Boukerche <mohamedboukerche55@gmail.com>
2025-11-05 23:06:30 +0100

    Create code1.sh

---
+++ code1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+# trying to print with colors -yellow 
+
+echo -e "$yellow >hello i am mohamed yellow"
+echo -e "$green >hello i am mohamed green"
+echo -e "$orange >hello i am mohamed orange"
+echo -e "$red >hello i am mohamed red"
